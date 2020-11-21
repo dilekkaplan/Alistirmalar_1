ef6aed25731caca1633fd31885310af31c26b581
dilekkaplan <72886943+dilekkaplan@users.noreply.github.com>
2020-11-21 15:04:17 +0300

    Add files via upload

---
+++ alistirma_11 yeni.py
@@ -0,0 +1,7 @@
+for i in range(2,350):
+    if (125%i)==(200%i)==(350%i):
+        sayi=i
+
+print(sayi)
+
+        
---
+++ alistirma_13.py
@@ -0,0 +1,5 @@
+for i in range(10,100):
+    for j in range(10,100):
+        sayi=str(i)+str(j)
+        if int(sayi)/11==(i+j):
+            print(i,j)
